e17296653faebeb0b1d5996d8c992f43c2bfbbd1
Roland Hummel <roland.hummel@rio.cloud>
2026-02-26 11:10:13 +0100

    add rimraf, delete .git after cloning

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import fs from 'node:fs';
 
 import git from 'isomorphic-git';
 import http from 'isomorphic-git/http/node';
+import { rimraf } from 'rimraf';
+
+await rimraf('./foobar');
 
 await git.clone({
     fs,
@@ -10,3 +13,5 @@ await git.clone({
     url: 'https://github.com/rio-cloud/create-frontend',
     depth: 1,
 });
+
+await rimraf('./foobar/.git');
